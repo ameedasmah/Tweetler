70568e5ef7a2aba7a89c52710022af8fc8d7e792
ameedasmah <ameed.asmah@hotmail.com>
2021-03-16 02:45:20 +0200

    modifyed

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from likes.models import Like
 
+
 class LikeSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(source='user_id.name', read_only=True )
+    user = serializers.CharField(source='user_id.name', read_only=True)
+
     class Meta:
         model = Like
-        fields = ('id','user_id', 'tweet_id', 'user')
+        fields = '__all__'
